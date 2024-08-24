1c63596298d8e10728505959f4e9fe87850a9e31
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2024-08-23 21:03:34 -0300

    summary

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,6 @@ import {createClient} from "@supabase/supabase-js";
 import {processEnv} from "@next/env";
 
 export const supabase = createClient(
-    process.env.
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 )
